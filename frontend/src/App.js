3b2c948de0e54bf13510f955185a176c09c09ba8
Davidson Chua <davidson99here@gmail.com>
2023-05-19 11:57:19 +0800

    Added Front End

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from './HearMeOut-1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code>Work In Progres </code> 
+           <p> Stay Tuned </p>
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/YZDavid/HearMeOut"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About Our Project
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
