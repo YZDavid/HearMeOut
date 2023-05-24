936c4ca26e77f2eda2e24ecb154ea429cb32e157
YZDavid <85034436+YZDavid@users.noreply.github.com>
2023-05-25 02:22:15 +0800

    Added links to go between pages, logic to cache current summary, sqlite3 database

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for
 from transformers import pipeline
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 import pyttsx3
 import os
 
@@ -8,6 +9,19 @@ summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 engine = pyttsx3.init()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///output.db'
+db = SQLAlchemy(app)
+
+class Output(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    input_field = db.Column(db.Text, nullable=False)
+    output_field = db.Column(db.Text, nullable=False)
+    audio_path = db.Column(db.String(200))
+    date = db.Column(db.DateTime)    
+
+INPUT = None
+OUTPUT = None
+AUDIO = None
 
 @app.route("/")
 def index():
@@ -15,12 +29,25 @@ def index():
 
 @app.route("/summary", methods=["POST", "GET"])
 def summary():
-    input = request.form.get("input")
-    output = summarizer(input, max_length=250, min_length=100, do_sample=False)[0]['summary_text']
-    # filename is audio_timestamp in windows file friendly format, no illegal characters such as ':'
-    filename = f'audio_{datetime.now()}.mp3'.replace(':', '.')
-    engine.save_to_file(output, 'test.mp3')
-    engine.runAndWait()
-    # pyttsx3 is only able to create output files such as 'test.mp3' for some reason, just output as such and use os library to rename file
-    os.rename('test.mp3', f'static/{filename}')
-    return render_template("summary.html", input=input, output=output, audio=filename)
+    if request.method == "POST":
+        input = request.form.get("input")
+        output = summarizer(input, max_length=250, min_length=200, do_sample=False)[0]['summary_text']
+        # filename is audio_timestamp in windows file friendly format, no illegal characters such as ':'
+        filename = f'audio_{datetime.now()}.mp3'.replace(':', '.')
+        engine.save_to_file(output, 'test.mp3')
+        engine.runAndWait()
+        # pyttsx3 is only able to create output files such as 'test.mp3' for some reason, just output as such and use os library to rename file
+        os.rename('test.mp3', f'static/{filename}')
+        global INPUT, OUTPUT, AUDIO
+        INPUT, OUTPUT, AUDIO = input, output, filename
+        return render_template("summary.html", input=input, output=output, audio=filename)
+    else:
+        try:
+            return render_template("summary.html", input=INPUT, output=OUTPUT, audio=AUDIO)
+        except:
+            return render_template("summary.html")
+
+
+@app.route("/saved")
+def saved():
+    return render_template("saved.html")
